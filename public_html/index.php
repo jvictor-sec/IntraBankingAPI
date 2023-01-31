28c50b70914a7033ce1ea8a60880c13b745877fc
= <=>
2023-01-30 22:37:48 -0300

    Bug de requisição consertado

--- public_html/index.php
+++ public_html/index.php
@@ -1,30 +1,32 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
-header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: *");
+header("Content-Type: application/json");
 
-require_once '../vendor/autoload.php';
+require_once "../vendor/autoload.php";
 
-if($_GET['url']){
-    $url =  explode("/", $_GET['url']);
+if($_SERVER["REQUEST_URI"]){
+  $url =  explode("/", $_SERVER["REQUEST_URI"]);
+  array_splice($url, 0, 4);
 
-    if($url[0] === "api"){
-        array_shift($url);
+  if($url[0] === "api"){
+    array_shift($url);
 
-        $controller = 'App\Controllers\\'.ucfirst($url[0]).'Controller';
-        array_shift($url);
-        $method = strtolower($_SERVER['REQUEST_METHOD']);
+    $controller = 'App\Controllers\\'.ucfirst($url[0]).'Controller';
 
-        try{
-            $response = call_user_func_array(array(new $controller, $method), $url);
-            http_response_code(200);
-            echo json_encode(array('status'=>'success', 'data'=>$response), JSON_UNESCAPED_UNICODE);
-        }
-        catch(\Exception $e){
-            http_response_code(404);
-            echo json_encode(array('status'=>'error', 'data'=>$e->getMessage()), JSON_UNESCAPED_UNICODE);
-        }
+    array_shift($url);
 
+    $method = strtolower($_SERVER['REQUEST_METHOD']);
+
+    try{
+        $response = call_user_func_array(array(new $controller, $method), $url);
+        http_response_code(200);
+        echo json_encode(array('status'=>'success', 'data'=>$response), JSON_UNESCAPED_UNICODE);
     }
+    catch(\Exception $e){
+        http_response_code(404);
+        echo json_encode(array('status'=>'error', 'data'=>$e->getMessage()), JSON_UNESCAPED_UNICODE);
+    }
+  }
 }
