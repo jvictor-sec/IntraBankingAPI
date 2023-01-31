24f095854ec6ab772ba9fdd7abbec40a16d8d426
= <=>
2023-01-30 22:39:00 -0300

    Construção da tabela de usuário

---
+++ Database/IntraBankingDatabase.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS intrabanking_database;
+CREATE DATABASE intrabanking_database;
+USE intrabanking_database;
+
+DROP TABLE IF EXISTS user_table;
+CREATE TABLE user_table(
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(80) NOT NULL,
+    user_email VARCHAR(100) NOT NULL UNIQUE,
+    user_ddd CHAR(2) NOT NULL,
+    user_number CHAR(9) NOT NULL,
+    user_password VARCHAR(30) NOT NULL
+);
