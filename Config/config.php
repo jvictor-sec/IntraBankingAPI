77457d714079152f86900104a23a3928f3ffa417
= <=>
2023-01-30 22:39:51 -0300

    Estrutura do arquivo de configuração do projeto

---
+++ Config/config.php
@@ -0,0 +1,7 @@
+<?php
+  define("DB_DRIVE", "mysql");
+  define("DB_HOST", "localhost");
+  define("DB_NAME", "intrabanking_database");
+  define("DB_USER", "seu_usuário");
+  define("DB_PASSWORD", "sua_senha");
+?>
