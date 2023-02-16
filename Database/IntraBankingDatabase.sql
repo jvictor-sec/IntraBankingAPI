2915fcf73e908ea6b11e21a4f1b6523f69baa3e7
jvictort <jvteodorobrasil@gmail.com>
2023-02-16 16:22:09 -0300

    Criação da tabela de finanças

--- Database/IntraBankingDatabase.sql
+++ Database/IntraBankingDatabase.sql
@@ -11,3 +11,15 @@ CREATE TABLE user_table(
     user_number CHAR(9) NOT NULL,
     user_password VARCHAR(32) NOT NULL
 );
+
+DROP TABLE IF EXISTS finance_table;
+CREATE TABLE finance_table (
+  finance_id INT PRIMARY KEY AUTO_INCREMENT,
+  finance_name VARCHAR(100) NOT NULL,
+  finance_description VARCHAR(250),
+  finance_price DECIMAL(19,4) NOT NULL,
+  finance_date DATE NOT NULL,
+  finance_recipient VARCHAR(100),
+  fk_user INT NOT NULL,
+  FOREIGN KEY (fk_user) REFERENCES user_table(user_id)
+);
